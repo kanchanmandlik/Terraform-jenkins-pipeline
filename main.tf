fcb04ad8c1a31e768846fed650e9edb8860ef178
kanchanmandlik <107538364+kanchanmandlik@users.noreply.github.com>
2026-05-25 20:30:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,59 @@
 
-
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "6.23.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.23.0"
     }
+  }
 }
 
-provider "aws"{
-    region = var.region
-
-
+provider "aws" {
+  region = var.region
 }
 
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name = "web-security-group"
+
+  # SSH Access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP Access
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["66.249.66.42/32"]
+  }
+
+  # Outbound Traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
 resource "aws_instance" "mywebserver" {
-    ami = "ami-0e12ffc2dd465f6e4"
-    instance_type = "t3.micro"
+  ami                    = "ami-0e12ffc2dd465f6e4"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-ingress { 
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["66.249.66.42"]
+  tags = {
+    Name = "sample-server"
+  }
+}
 
-
-    }
-
-    tags = {
-        Name = "sample server"
-        
-    }
-  
+# Output Public IP
+output "aws_instance-public_ip" {
+  value = aws_instance.mywebserver.public_ip
 }
 

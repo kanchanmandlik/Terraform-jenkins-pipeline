e8c63ec06f866f54aac580fe40299f7e23a2b88c
kanchanmandlik <107538364+kanchanmandlik@users.noreply.github.com>
2026-05-25 20:49:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,33 +12,6 @@ provider "aws" {
   region = var.region
 }
 
-# Security Group
-resource "aws_security_group" "web_sg" {
-  name = "web-security-group"
-
-  # SSH Access
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTP Access
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["66.249.66.42/32"]
-  }
-
-  # Outbound Traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 # EC2 Instance
@@ -46,7 +19,7 @@ resource "aws_instance" "mywebserver" {
   ami                    = "ami-0e12ffc2dd465f6e4"
   instance_type          = "t3.micro"
   key_name               = "mynewkey"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [vpc_security_group_ids = ["sg-05699f7bb60e9f9e0"]]
 
   tags = {
     Name = "sample-server"

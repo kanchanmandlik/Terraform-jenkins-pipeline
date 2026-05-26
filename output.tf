7f1fef133b5ab943e39a71562dad100074a3e6d4
kanchanmandlik <107538364+kanchanmandlik@users.noreply.github.com>
2026-05-26 20:14:27 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-# Output existing or new instance ID
 output "instance_id" {
+  value = length(data.aws_instances.existing.ids) > 0 ? data.aws_instances.existing.ids[0] : aws_instance.mywebserver[0].id
+}
 
-  value = length(data.aws_instances.existing.ids) > 0 ?
-    data.aws_instances.existing.ids[0] :
-    aws_instance.mywebserver[0].id
+output "public_ip" {
+  value = length(data.aws_instances.existing.ids) > 0 ? "Using Existing Server" : aws_instance.mywebserver[0].public_ip
 }
